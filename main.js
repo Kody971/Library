417b57e3e13d1bfb445499258b7c7ec1cb8fad29
Ayub Khoirudin <khoirudinayub971@gmail.com>
2026-07-03 07:37:19 +0700

    Add some class

--- main.js
+++ main.js
@@ -15,26 +15,6 @@ let myLibrary = [
   },
 ];
 
-class Book {
-  constructor(name, author, pages, readed) {
-    this.id = crypto.randomUUID();
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.readed = readed;
-  }
-}
-
-class Library {
-  constructor(books = []) {
-    this.books = books;
-  }
-}
-
-class Display {
-  constructor() {}
-}
-
 function Book(name, author, pages, readed) {
   this.id = crypto.randomUUID();
   this.name = name;
@@ -135,3 +115,87 @@ const addBtn = document.querySelector("form");
 addBtn.addEventListener("submit", addBookToLibrary);
 
 loadPage();
+
+class Book {
+  constructor(name, author, pages, readed) {
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.readed = readed;
+  }
+}
+
+class Library {
+  constructor(books = []) {
+    this.books = books;
+  }
+
+  addBook(book) {
+    this.books.push(book);
+  }
+
+  deleteBook() {}
+
+  getBooks() {
+    return this.books;
+  }
+}
+
+class Display {
+  render(books) {
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+
+    books.forEach((book) => {
+      const main = document.querySelector("main");
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const id = document.createElement("input");
+      id.setAttribute("type", "hidden");
+      id.setAttribute("id", "userId");
+      id.setAttribute("name", "userId");
+      id.setAttribute("value", book.id);
+      card.appendChild(id);
+
+      const container1 = document.createElement("div");
+      container1.classList.add("container-detail");
+
+      const mark = document.createElement("p");
+      mark.classList.add("mark");
+      if (book.read) {
+        mark.classList.add("true");
+        mark.textContent = "Readed";
+      } else {
+        mark.classList.add("false");
+        mark.textContent = "Unreaded";
+      }
+      container1.appendChild(mark);
+
+      const name = document.createElement("h2");
+      name.textContent = book.name;
+      container1.appendChild(name);
+
+      const author = document.createElement("p");
+      author.textContent = book.author;
+      container1.appendChild(author);
+
+      const pages = document.createElement("p");
+      pages.textContent = book.pages + " pages";
+      container1.appendChild(pages);
+
+      const container2 = document.createElement("div");
+
+      const removeBtn = document.createElement("button");
+      removeBtn.classList.add("remove-btn");
+      removeBtn.textContent = "Remove";
+      removeBtn.addEventListener("click", removeBookFromLibrary);
+      container2.appendChild(removeBtn);
+
+      card.appendChild(container1);
+      card.appendChild(container2);
+      main.appendChild(card);
+    });
+  }
+}
